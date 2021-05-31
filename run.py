54b55ca60548db5c102be7a4195e30eca2748578
vikasinder <narinder_sharma1982@yahoo.com>
2021-05-31 19:02:07 +0000

    updations

---
+++ run.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')  # http://127.0.0.1:5000/
+def home():
+ 
+    return render_template('index.html')
+
+# to embed python code in html we use jinja technique
+
+
+@app.route('/aboutus')  # http://127.0.0.1:5000/about_us
+def aboutus():
+    return render_template('aboutus.html')
+
+
+@app.route('/services')  # http://127.0.0.1:5000/about_us
+def services():
+    return render_template('services.html')
+
+
+@app.route('/contact')  # http://127.0.0.1:5000/services
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
